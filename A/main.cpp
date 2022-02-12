4221b043c66f96cd331394f202fa663c27742530
conputerEducTor <80802776+conputerEducTor@users.noreply.github.com>
2022-02-12 21:32:28 +0530

    A1

--- A/main.cpp
+++ A/main.cpp
@@ -1,7 +1,9 @@
 /* A1 Consider telephone book database of N clients. Make use of a hash table implementation
 to quickly look up client‘s telephone number. Make use of two collision handling
 techniques and compare them using number of comparisons required to find a set of
-telephone numbers*/
+telephone numbers
+
+!-----> Using Separate Chaning Method <--------*/
 
 
 
